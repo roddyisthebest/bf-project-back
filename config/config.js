01230e4793ca9fddeb9c64fe31a614792bbe553d
배성연 <baeseong-yeon@baeseong-yeon-ui-MacBookAir.local>
2022-02-24 21:07:00 +0900

    make it better

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ module.exports = {
   prod: {
     host: "us-cdbr-east-05.cleardb.net",
     user: "b8dabc4d000c47",
-    password: process.env.SEQUELIZE_PASSWORD,
+    password: "1c21a92d",
     database: "heroku_7f8e72880db0558",
     dialect: "mysql",
     timezone: "+09:00",
