0fb1cc27410421628b0f897ea0bba80f92cc8ced
배성연 <baeseong-yeon@baeseong-yeon-ui-MacBookAir.local>
2022-02-24 22:02:40 +0900

    final

--- config/config.js
+++ config/config.js
@@ -11,10 +11,10 @@ module.exports = {
   },
 
   prod: {
-    host: "us-cdbr-east-05.cleardb.net",
-    user: "b8dabc4d000c47",
-    password: "1c21a92d",
-    database: "heroku_7f8e72880db0558",
+    username: "root",
+    password: "bsy30228",
+    database: "bf",
+    host: "127.0.0.1",
     dialect: "mysql",
     timezone: "+09:00",
   },
