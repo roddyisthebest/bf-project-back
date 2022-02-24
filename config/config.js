432d4ca0e98286e1a3d30ee02364eca8af236636
배성연 <baeseong-yeon@baeseong-yeon-ui-MacBookAir.local>
2022-02-24 20:41:04 +0900

    make it better

---
+++ config/config.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "root",
+    password: process.env.SEQUElIZE_PASSWORD,
+    database: "bf",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    timezone: "+09:00",
+  },
+  prod: {
+    host: "us-cdbr-east-05.cleardb.net",
+    user: "b8dabc4d000c47",
+    password: process.env.SEQUElIZE_PASSWORD,
+    database: "heroku_7f8e72880db0558",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    timezone: "+09:00",
+  },
+};
