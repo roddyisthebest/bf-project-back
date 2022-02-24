9d630d20e9dc2a9122411da23de89ade733bd43f
배성연 <baeseong-yeon@baeseong-yeon-ui-MacBookAir.local>
2022-02-24 20:57:43 +0900

    make it better

--- config/config.js
+++ config/config.js
@@ -3,16 +3,17 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: "root",
-    password: process.env.SEQUElIZE_PASSWORD,
+    password: "bsy30228",
     database: "bf",
     host: "127.0.0.1",
     dialect: "mysql",
     timezone: "+09:00",
   },
-  prod: {
+
+  production: {
     host: "us-cdbr-east-05.cleardb.net",
     user: "b8dabc4d000c47",
-    password: process.env.SEQUElIZE_PASSWORD,
+    password: process.env.SEQUELIZE_PASSWORD,
     database: "heroku_7f8e72880db0558",
     dialect: "mysql",
     timezone: "+09:00",
